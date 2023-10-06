ec4b8f3ef83d230b10409703e07ef8caa7690b16
elan5027 <elan3050@gmail.com>
2023-10-06 18:01:22 +0900

    TST!

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
+from st_files_connection import FilesConnection
+
+
+conn = st.experimental_connection('s3', type=FilesConnection)
+st.info(conn)
+df = conn.read("TEST.png", input_format="png", ttl=600)
 
 st.info("파일선택")
 
 with st.sidebar:
-    selected = option_menu("Menu", ["테스트옵션001", "테스트옵션002"], menu_icon="cast", default_index=0, styles={
-        "container": {"padding": "4!important", "background-color": "#fafafa"},
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#08c7b4"}})
+    st.info("TEST Sidebar")
+
     
