f9dc5897b2f16fdd2c4b9857f82f6f03315ee3b9
elan5027 <elan3050@gmail.com>
2023-10-06 16:58:53 +0900

    test1

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu("Menu", ["테스트옵션001", "테스트옵션002"], menu_icon="cast", default_index=0, styles={
-        "container": {"padding": "4!important", "background-color": "#fafafa"},
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-        "nav-link-selected": {"background-color": "#08c7b4"}})
+st.selectbox("파일선택")
+
+# with st.sidebar:
+#     selected = option_menu("Menu", ["테스트옵션001", "테스트옵션002"], menu_icon="cast", default_index=0, styles={
+#         "container": {"padding": "4!important", "background-color": "#fafafa"},
+#         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
+#         "nav-link-selected": {"background-color": "#08c7b4"}})
     
